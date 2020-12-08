172fa286522875cc19b47f3f4433cacefb1b2190
Tien <tien2525@gmail.com>
2020-12-09 03:23:59 +0800

    getting html

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "http://www.twitter.com"
+response = requests.get(url, timeout=5)
+content = BeautifulSoup(response.content, "html.parser")
+
 if __name__ == "__main__":
-    print("hello world")
+    print(content)
